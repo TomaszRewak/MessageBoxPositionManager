6bc6ecc8053fc81ee79407537e9d16bcd19416ef
Tomasz Rewak <tomaszrewak@gmail.com>
2020-11-07 20:57:10 +0100

    Add project files.

---
+++ MessageBoxPositionManager/MessageBoxInterop.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MessageBoxPositionManager
+{
+	internal class MessageBoxInterop
+	{
+		[DllImport("kernel32.dll")]
+		public static extern uint GetCurrentThreadId();
+
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct RECT
+		{
+			public int Left;        // x position of upper-left corner
+			public int Top;         // y position of upper-left corner
+			public int Right;       // x position of lower-right corner
+			public int Bottom;      // y position of lower-right corner
+		};
+
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern IntPtr SetWindowsHookEx(HookType hookType, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+		public delegate IntPtr HookProc(int code, IntPtr wParam, IntPtr lParam);
+
+		[DllImport("user32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, SetWindowPosFlags uFlags);
+
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+		[DllImport("user32.dll")]
+		public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+		[DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+		public static extern IntPtr GetParent(IntPtr hWnd);
+
+		[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+		public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+		private const int HWND_TOP = 0;
+		private const int HWND_BOTTOM = 1;
+		private const int HWND_TOPMOST = -1;
+		private const int HWND_NOTOPMOST = -2;
+
+		public enum HookType : int
+		{
+			WH_JOURNALRECORD = 0,
+			WH_JOURNALPLAYBACK = 1,
+			WH_KEYBOARD = 2,
+			WH_GETMESSAGE = 3,
+			WH_CALLWNDPROC = 4,
+			WH_CBT = 5,
+			WH_SYSMSGFILTER = 6,
+			WH_MOUSE = 7,
+			WH_HARDWARE = 8,
+			WH_DEBUG = 9,
+			WH_SHELL = 10,
+			WH_FOREGROUNDIDLE = 11,
+			WH_CALLWNDPROCRET = 12,
+			WH_KEYBOARD_LL = 13,
+			WH_MOUSE_LL = 14
+		}
+
+		public enum SetWindowPosFlags : uint
+		{
+			/// <summary>If the calling thread and the thread that owns the window are attached to different input queues,
+			/// the system posts the request to the thread that owns the window. This prevents the calling thread from
+			/// blocking its execution while other threads process the request.</summary>
+			/// <remarks>SWP_ASYNCWINDOWPOS</remarks>
+			SynchronousWindowPosition = 0x4000,
+			/// <summary>Prevents generation of the WM_SYNCPAINT message.</summary>
+			/// <remarks>SWP_DEFERERASE</remarks>
+			DeferErase = 0x2000,
+			/// <summary>Draws a frame (defined in the window's class description) around the window.</summary>
+			/// <remarks>SWP_DRAWFRAME</remarks>
+			DrawFrame = 0x0020,
+			/// <summary>Applies new frame styles set using the SetWindowLong function. Sends a WM_NCCALCSIZE message to
+			/// the window, even if the window's size is not being changed. If this flag is not specified, WM_NCCALCSIZE
+			/// is sent only when the window's size is being changed.</summary>
+			/// <remarks>SWP_FRAMECHANGED</remarks>
+			FrameChanged = 0x0020,
+			/// <summary>Hides the window.</summary>
+			/// <remarks>SWP_HIDEWINDOW</remarks>
+			HideWindow = 0x0080,
+			/// <summary>Does not activate the window. If this flag is not set, the window is activated and moved to the
+			/// top of either the topmost or non-topmost group (depending on the setting of the hWndInsertAfter
+			/// parameter).</summary>
+			/// <remarks>SWP_NOACTIVATE</remarks>
+			DoNotActivate = 0x0010,
+			/// <summary>Discards the entire contents of the client area. If this flag is not specified, the valid
+			/// contents of the client area are saved and copied back into the client area after the window is sized or
+			/// repositioned.</summary>
+			/// <remarks>SWP_NOCOPYBITS</remarks>
+			DoNotCopyBits = 0x0100,
+			/// <summary>Retains the current position (ignores X and Y parameters).</summary>
+			/// <remarks>SWP_NOMOVE</remarks>
+			IgnoreMove = 0x0002,
+			/// <summary>Does not change the owner window's position in the Z order.</summary>
+			/// <remarks>SWP_NOOWNERZORDER</remarks>
+			DoNotChangeOwnerZOrder = 0x0200,
+			/// <summary>Does not redraw changes. If this flag is set, no repainting of any kind occurs. This applies to
+			/// the client area, the nonclient area (including the title bar and scroll bars), and any part of the parent
+			/// window uncovered as a result of the window being moved. When this flag is set, the application must
+			/// explicitly invalidate or redraw any parts of the window and parent window that need redrawing.</summary>
+			/// <remarks>SWP_NOREDRAW</remarks>
+			DoNotRedraw = 0x0008,
+			/// <summary>Same as the SWP_NOOWNERZORDER flag.</summary>
+			/// <remarks>SWP_NOREPOSITION</remarks>
+			DoNotReposition = 0x0200,
+			/// <summary>Prevents the window from receiving the WM_WINDOWPOSCHANGING message.</summary>
+			/// <remarks>SWP_NOSENDCHANGING</remarks>
+			DoNotSendChangingEvent = 0x0400,
+			/// <summary>Retains the current size (ignores the cx and cy parameters).</summary>
+			/// <remarks>SWP_NOSIZE</remarks>
+			IgnoreResize = 0x0001,
+			/// <summary>Retains the current Z order (ignores the hWndInsertAfter parameter).</summary>
+			/// <remarks>SWP_NOZORDER</remarks>
+			IgnoreZOrder = 0x0004,
+			/// <summary>Displays the window.</summary>
+			/// <remarks>SWP_SHOWWINDOW</remarks>
+			ShowWindow = 0x0040,
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct Msg
+		{
+			public readonly IntPtr LResult;
+			public readonly IntPtr LParam;
+			public readonly IntPtr WParam;
+			public readonly uint Message;
+			public readonly IntPtr Hwnd;
+		}
+	}
+}
---
+++ MessageBoxPositionManager/MessageBoxPositionManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using static MessageBoxPositionManager.MessageBoxInterop;
+
+namespace MessageBoxPositionManager
+{
+	public static class MessageBoxPositionManager
+	{
+		[ThreadStatic] private static IntPtr _hook;
+		[ThreadStatic] private static HookProc _hookProc;
+
+		public static void Initialize()
+		{
+			if (_hook != IntPtr.Zero) return;
+
+			_hookProc = HandleMessage;
+			_hook = SetWindowsHookEx(HookType.WH_CALLWNDPROCRET, _hookProc, IntPtr.Zero, GetCurrentThreadId());
+		}
+
+		private static IntPtr HandleMessage(int code, IntPtr wParam, IntPtr lParam)
+		{
+			if (code >= 0)
+				ProcessMessage(lParam);
+
+			return CallNextHookEx(_hook, code, wParam, lParam);
+		}
+
+		private static void ProcessMessage(IntPtr param)
+		{
+			var message = Marshal.PtrToStructure<Msg>(param);
+			if (message.Message != 0x0018) return;
+
+			var box = message.Hwnd;
+			var parent = GetParent(box);
+			if (param == IntPtr.Zero) return;
+
+			var className = new StringBuilder(7);
+			GetClassName(box, className, className.Capacity);
+			if (!className.Equals("#32770")) return;
+
+			if (!GetWindowRect(box, out var boxRect)) return;
+			if (!GetWindowRect(parent, out var parentRect)) return;
+
+			var width = boxRect.Right - boxRect.Left;
+			var height = boxRect.Bottom - boxRect.Top;
+			var left = (parentRect.Left + parentRect.Right) / 2 - width / 2;
+			var top = (parentRect.Top + parentRect.Bottom) / 2 - height / 2;
+
+			const SetWindowPosFlags commonFlags =
+				SetWindowPosFlags.SynchronousWindowPosition |
+				SetWindowPosFlags.IgnoreResize |
+				SetWindowPosFlags.DoNotActivate |
+				SetWindowPosFlags.DoNotChangeOwnerZOrder |
+				SetWindowPosFlags.IgnoreZOrder;
+
+			SetWindowPos(box, IntPtr.Zero, left, top, width, height, commonFlags);
+		}
+	}
+}
