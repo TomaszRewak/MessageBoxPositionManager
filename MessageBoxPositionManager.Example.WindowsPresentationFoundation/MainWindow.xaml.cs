173a706a0f434bb29e4dbc8a7f45897918fe5dd0
Tomasz Rewak <tomaszrewak@gmail.com>
2020-11-07 21:17:37 +0100

    Adding WPF example.

---
+++ MessageBoxPositionManager.Example.WindowsPresentationFoundation/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MessageBoxPositionManager.Example.WindowsPresentationFoundation
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void NewMessageBoxButton_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show(this, "Some content", "Title", MessageBoxButton.OK);
+		}
+
+		private void NewWindowButton_Click(object sender, RoutedEventArgs e)
+		{
+			var newWindow = new MainWindow();
+			newWindow.Show();
+		}
+	}
+}
